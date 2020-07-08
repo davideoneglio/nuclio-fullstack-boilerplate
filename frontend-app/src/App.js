3468e33f828323497336009bb27ed19a240aaa87
aleixff <aaleeiix@gmail.com>
2020-07-08 19:20:07 +0200

    Add HomePage

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import LandingPage from "./Components/LandingPage/LandingPage";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import {Signup} from "./Components/SignupPage/signuppage";
+import HomePage from "./Components/HomePage/HomePage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             <Switch>
                 <Route exact path="/" render={props => <LandingPage {...props}/>}/>
                 <Route exact path="/signup" render={props => <Signup {...props}/>}/>
+                <Route exact path="/home" render={props => <HomePage {...props}/>}/>
             </Switch>
         </div>
       </Router>
