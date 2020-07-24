499d3af688d7859f134ec3b46d24c24ba59c6eab
pepef19 <pepefaura19@gmail.com>
2020-07-24 20:13:34 +0200

    a

--- frontend-app/src/index.js
+++ frontend-app/src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./App";
-import store from "./Components/InitialBoard/store";
-import * as serviceWorker from './serviceWorker';
 
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
